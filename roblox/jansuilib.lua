d41a7a9e5c02c2f842d9e8d595a76e9b8bcead88
Jay <94145603+JayGitCuh@users.noreply.github.com>
2022-06-28 09:32:55 -0700

    Update jansuilib.lua

--- roblox/jansuilib.lua
+++ roblox/jansuilib.lua
@@ -11,7 +11,9 @@ if getgenv().library then
     getgenv().library:Unload()
 end
 
-local library = {design = getgenv().design == "kali" and "kali" or "uwuware", tabs = {}, draggable = true, flags = {}, title = "CheatX", open = false, popup = nil, instances = {}, connections = {}, options = {}, notifications = {}, tabSize = 0, theme = {}, foldername = "cheatx_cnfgs", fileext = ".txt"}
+
+
+local library = {design = getgenv().design == "kali" and "kali" or "uwuware", tabs = {}, draggable = true, flags = {}, title = getgenv.menu["name"], open = false, popup = nil, instances = {}, connections = {}, options = {}, notifications = {}, tabSize = 0, theme = {}, foldername = getgenv.menu["config"], fileext = ".txt"}
 getgenv().library = library
 
 --Locals
